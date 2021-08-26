a3fb2796dd18e8bda67d6348250960a561ad5357
Angelo Brocca <84419409+AngeloBrocca@users.noreply.github.com>
2021-08-26 13:17:09 -0300

    Update e1.py

--- Exercicios 4/e1.py
+++ Exercicios 4/e1.py
@@ -16,14 +16,14 @@ banco = input('Digite aqui número do banco: ')
 valor = input('Digite o valor a ser pago: ')
 data = input('Insira a data do registro(dd/mm/aaaa): ')
 
-if(banco == '7'):
-    banco = '7 - Itaú'
+if(banco == '3'):
+    banco = '3 - Itaú'
 elif(banco == '1'):
     banco = '1 - Banco do Brasil'
 elif(banco == '2'):
     banco = '2 - Caixa'
-elif(banco == '3'):
-    banco = '3 - Bradesco'
+elif(banco == '4'):
+    banco = '4 - Bradesco'
 else:
     try:
         raise Exception
